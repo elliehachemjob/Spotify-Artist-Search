b8167e680a0799d296be8f7308198d8120cc7487
elliehachemjob <elliehachemjob@gmail.com>
2021-11-25 10:07:03 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const SPOTIFY_ENDPOINT_AUTHORIZATION =
+    "https://accounts.spotify.com/authorize";
+
+  const CLIENT_ID: any = "eae2c519c3084b16a48056ce021a5ae2";
+  const REDIRECT_URI: any = "http://localhost:3000/";
+  const SCOPES: any = [
+    "user-read-playback-state",
+    "user-read-currently-playing",
+  ];
+  const DELIMITER: any = "%20";
+  const SCOPE_URI_PARAM: any = SCOPES.join(DELIMITER);
+
+  const handleLogin = () => {
+    // @ts-ignore
+    window.location = `${SPOTIFY_ENDPOINT_AUTHORIZATION}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE_URI_PARAM}&response_type=token&show_dialog=true`;
+  };
+
+  /*
+expected Url: http://localhost:3000/#access_token=BQADfrbQPCfRd-1FPIdzEs6LkLpWSIxmu-f5iMzxhhRU7uvFVysPOgZwsgU8IgPS7T1CzUbS8gv1_KpYQ_kaSSeZ5aL8mPswB7SdlKhilqsDLp1YfNj-jRgpZA3ogPNTZgCJNYg4YVJXQDHRC-g7cdTZ-k9HEROgnnFBCpA&token_type=Bearer&expires_in=3600
+*/
+
+  const getReturnParamsFromSpotifyAuth = (hash: any) => {
+    const stringAfterHash = hash.substring(1);
+    const paramsInUrl = stringAfterHash.split("&");
+    const paramsSplitUp = paramsInUrl.reduce(
+      (accumulator: any, currentValue: any) => {
+        console.log(currentValue);
+        const [key, value] = currentValue.split("=");
+        accumulator[key] = value;
+        return accumulator;
+      },
+      {}
+    );
+    return paramsSplitUp;
+  };
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnParamsFromSpotifyAuth(window.location.hash);
+      console.log({ access_token });
+      localStorage.clear();
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("expiresIn", expires_in);
+      localStorage.setItem("tokenType", token_type);
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
